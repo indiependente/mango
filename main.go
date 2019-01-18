7f7825862c047154ab708f8e17d8c1544adf27c0
Francesco Farina <rockerg991@gmail.com>
2019-01-18 00:12:53 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const (
+	urlTemplate = "https://www.mangaeden.com/it/it-manga/shingeki-no-kyojin/CHAP/1/"
+)
+
+func main() {
+	manga := os.Args[1]
+	manga = strings.ToLower(manga)
+	manga = strings.Replace(manga, " ", "-", -1)
+	chap := os.Args[2]
+	url := strings.Replace(urlTemplate, "CHAP", chap, -1)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Panicf("Could not GET %s\n: %v\n", url, err)
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicf("Could not read Body: %v\n", err)
+	}
+	fmt.Println(string(data))
+}
