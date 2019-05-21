c9fe94fc99988fb944755b4e0a13d7b5107244f1
Francesco Farina <rockerg991@gmail.com>
2019-05-22 00:10:42 +0100

    mvp working

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/gocolly/colly"
 )
@@ -28,25 +29,43 @@ func main() {
 		colly.Async(true),
 	)
 	c.Limit(&colly.LimitRule{
-		Parallelism: 2,
-		RandomDelay: 5 * time.Second,
+		DomainGlob:  "*mangaeden.*",
+		Parallelism: 5,
 	})
-	pagesColl := colly.NewCollector()
-	// pagesColl.Limit(&colly.LimitRule{
-	// 	Parallelism: 2,
-	// 	RandomDelay: 5 * time.Second,
-	// })
-
-	pagesColl.OnRequest(func(r *colly.Request) {
-		fmt.Println(">", r.URL.String())
+	pagesColl := colly.NewCollector(
+		colly.Async(true),
+	)
+	pagesColl.Limit(&colly.LimitRule{
+		DomainGlob:  "*mangaeden.*",
+		Parallelism: 5,
 	})
 
 	pagesColl.OnError(func(r *colly.Response, err error) {
-		fmt.Println("pagesColl failed- URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+		fmt.Println("pagesColl failed- URL:", r.Request.URL, "\nError:", err)
 	})
 
 	pagesColl.OnHTML("#mainImg", func(e *colly.HTMLElement) {
-		fmt.Printf("%s\n", e.Attr("src"))
+		altFields := strings.Fields(e.Attr("alt"))
+		pageNum := altFields[len(altFields)-1]
+
+		resp, err := http.Get("https:" + e.Attr("src"))
+		if err != nil {
+			fmt.Printf("Failed to get %s: %v\n", "https"+e.Attr("src"), err)
+			return
+		}
+		defer resp.Body.Close()
+		f, err := os.Create(manga + "_" + chap + "_" + pageNum + ".png")
+		if err != nil {
+			fmt.Printf("Failed to create file: %v\n", err)
+			return
+		}
+		defer f.Close()
+		_, err = io.Copy(f, resp.Body)
+		if err != nil {
+			fmt.Printf("Could not copy response content: %v\n", err)
+			return
+		}
+
 	})
 
 	c.OnHTML("#pageSelect", func(e *colly.HTMLElement) {
@@ -56,10 +75,10 @@ func main() {
 		})
 	})
 	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("c failed- URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+		fmt.Println("c failed- URL:", r.Request.URL, "\nError:", err)
 	})
 
 	c.Visit(base + url)
 	c.Wait()
-
+	pagesColl.Wait()
 }
